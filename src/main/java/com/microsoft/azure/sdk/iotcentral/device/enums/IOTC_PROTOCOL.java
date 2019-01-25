4f2656b2ca4250cc4868134bbbb7e903bc65b592
lucadruda <luca.druda@solaircorporate.com>
2019-01-25 15:51:11 +0100

    update license

--- src/main/java/com/microsoft/azure/sdk/iotcentral/device/enums/IOTC_PROTOCOL.java
+++ src/main/java/com/microsoft/azure/sdk/iotcentral/device/enums/IOTC_PROTOCOL.java
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Luca Druda. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 package com.microsoft.azure.sdk.iotcentral.device.enums;
 public enum IOTC_PROTOCOL {
